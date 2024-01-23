1a5c5739cb6f484b117a343eb4709c937d25166b
Jimmie Max <mayiming@morefungroup.com>
2024-01-23 12:01:56 +0800

    chore(init): console version

--- lib/creater.ts
+++ lib/creater.ts
@@ -1,10 +1,10 @@
-import { CreateOption, CreaterStrategy } from "./creater.type";
-import * as fs from "fs";
-import * as path from "path";
-import minimist from "minimist";
-import { copy, emptyDir, hasDir } from "./utils/file";
-import { PromptsStep } from "./prompts-step";
-import { green } from "kolorist";
+import { CreateOption, CreaterStrategy } from './creater.type';
+import * as fs from 'fs';
+import * as path from 'path';
+import minimist from 'minimist';
+import { copy, emptyDir, hasDir } from './utils/file';
+import { PromptsStep } from './prompts-step';
+import { green } from 'kolorist';
 import { URL } from 'url';
 const __dirname = new URL('.', import.meta.url).pathname;
 const cwd = process.cwd();
@@ -19,19 +19,19 @@ export class Creater {
   private strategy?: CreaterStrategy;
   public dependencies = {};
   public devDependencies = {
-    "@commitlint/cli": "^17.4.4",
-    "@commitlint/config-conventional": "^17.6.7",
-    "@geektech/commitlint-config": "^0.0.2",
-    prettier: "^3.0.0",
-    husky: "^8.0.3",
-    "lint-staged": "^13.2.3",
+    '@commitlint/cli': '^17.4.4',
+    '@commitlint/config-conventional': '^17.6.7',
+    '@geektech/commitlint-config': '^0.0.2',
+    prettier: '^3.0.0',
+    husky: '^8.0.3',
+    'lint-staged': '^13.2.3',
   };
   public scripts = {
-    "lint-staged": "lint-staged",
-    prepare: "husky install",
+    'lint-staged': 'lint-staged',
+    prepare: 'husky install',
   };
   public lintStaged = {
-    "*.{js,jsx,ts,tsx,vue,css,less,json,md}": ["prettier --write"],
+    '*.{js,jsx,ts,tsx,vue,css,less,json,md}': ['prettier --write'],
   };
 
   constructor(public strategies: CreaterStrategy[]) {
@@ -39,6 +39,8 @@ export class Creater {
   }
 
   async init() {
+    const p = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+    console.log(`create-start version: ${p.version}\n`);
     const { framework, overwrite, projectName, variant } =
       await this.getPromptsResult();
     this.setStrategy(framework.name);
@@ -49,7 +51,7 @@ export class Creater {
       template: variant,
       overwrite,
       framework,
-      packageJson: {}
+      packageJson: {},
     };
     await this.pre(createOption);
     await this.create(createOption);
@@ -58,13 +60,13 @@ export class Creater {
 
   setStrategy(strategyName: string) {
     this.strategy = this.strategies.find(
-      (strategy) => strategy.name === strategyName
+      strategy => strategy.name === strategyName,
     );
   }
 
   pre(option: CreateOption) {
     if (option.overwrite) {
-      emptyDir(option.root, [".git"]);
+      emptyDir(option.root, ['.git']);
     } else if (!fs.existsSync(option.root)) {
       fs.mkdirSync(option.root);
     }
@@ -83,7 +85,7 @@ export class Creater {
     console.log(`\nDone. Now run:\n`);
     console.log(`  cd ${path.relative(cwd, option.root)}`);
     const hasInitGit = hasDir('.git', option.root);
-    if(!hasInitGit) {
+    if (!hasInitGit) {
       console.log(`  First of all, you have to init git file`);
       console.log(`  Run: git init`);
       console.log(`  Run: git remote add origin <origin git path>`);
@@ -94,13 +96,13 @@ export class Creater {
 
   async getPromptsResult() {
     const argv = minimist(process.argv.slice(2), {
-      string: ["_"],
+      string: ['_'],
     });
     const targetDir: string = argv._[0];
     let template = argv.template || argv.t;
-    const defaultProjectName = targetDir || "project-starter";
+    const defaultProjectName = targetDir || 'project-starter';
     const templates = this.strategies
-      .map((f) => (f.variants && f.variants?.map((v) => v.name)) || [f.name])
+      .map(f => (f.variants && f.variants?.map(v => v.name)) || [f.name])
       .reduce((a, b) => a.concat(b), []);
     const promptsStep = new PromptsStep(targetDir);
     promptsStep.addProjectNameStep(defaultProjectName);
@@ -118,26 +120,26 @@ export class Creater {
 
   initPackage(option: CreateOption) {
     const targetPackagePath = path.join(option.root, `package.json`);
-    const packageJson = JSON.parse(fs.readFileSync(targetPackagePath, "utf8"));
+    const packageJson = JSON.parse(fs.readFileSync(targetPackagePath, 'utf8'));
     packageJson.name = option.projectName;
     packageJson.dependencies = {
-      ...packageJson.dependencies ?? {},
-      ...this.dependencies ?? {},
+      ...(packageJson.dependencies ?? {}),
+      ...(this.dependencies ?? {}),
       ...(this.strategy.dependencies ?? {}),
     };
     packageJson.devDependencies = {
-      ...packageJson.devDependencies ?? {},
-      ...this.devDependencies ?? {},
+      ...(packageJson.devDependencies ?? {}),
+      ...(this.devDependencies ?? {}),
       ...(this.strategy.devDependencies ?? {}),
     };
     packageJson.scripts = {
-      ...packageJson.scripts ?? {},
-      ...this.scripts ?? {},
+      ...(packageJson.scripts ?? {}),
+      ...(this.scripts ?? {}),
       ...(this.strategy.scripts ?? {}),
     };
-    packageJson["lint-staged"] = {
-      ...packageJson.lintStaged ?? {},
-      ...this.lintStaged ?? {},
+    packageJson['lint-staged'] = {
+      ...(packageJson.lintStaged ?? {}),
+      ...(this.lintStaged ?? {}),
       ...(this.strategy.lintStaged ?? {}),
     };
     option.packageJson = packageJson;
@@ -145,9 +147,9 @@ export class Creater {
   }
 
   initExtraFiles(option: CreateOption) {
-    copy(option.packageRoot + "strategies/common", option.root);
-    const sourcePath = option.packageRoot +
-      `strategies/${option.framework.path}/extra`;
+    copy(option.packageRoot + 'strategies/common', option.root);
+    const sourcePath =
+      option.packageRoot + `strategies/${option.framework.path}/extra`;
     if (fs.existsSync(sourcePath)) {
       copy(sourcePath, option.root);
     }
