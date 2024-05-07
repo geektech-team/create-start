6e5867e5e77f6de45d7ca734dc06180e32e25e77
Jimmie Max <mayiming@morefungroup.com>
2024-05-07 16:51:47 +0800

    feat(vue): catch login expired emit

---
+++ lib/strategies/vue/extra/src/http/error-code.ts
@@ -0,0 +1,3 @@
+export enum ErrorCode {
+  LoginExpired = 51555
+}
--- lib/strategies/vue/extra/src/http/interceptors/catch-login-expired.ts
+++ lib/strategies/vue/extra/src/http/interceptors/catch-login-expired.ts
@@ -1,13 +1,15 @@
 import { AxiosResponse } from 'axios';
+import { eventBus } from '@geektech/utils';
+import { ErrorCode } from '../error-code';
 
 export const catchLoginExpired = (response: AxiosResponse) => {
   const res = response.data;
   if (
     [
-      51555, // 登录认证失败 expired
+      ErrorCode.LoginExpired, // 登录认证失败 expired
     ].includes(res?.statusCode)
   ) {
-    location.href = '/login';
+    eventBus.emit('login-expired');
     return Promise.reject(new Error(res.comments || 'Error'));
   }
   return response;
