6e3bc057449bfa01e2cd0a1c713bb19e34b20ae2
Jimmie Max <mayiming@morefungroup.com>
2024-05-11 11:35:30 +0800

    feat(nest-core): update 3.2

--- lib/strategies/nest/index.ts
+++ lib/strategies/nest/index.ts
@@ -11,7 +11,7 @@ export class NestCreaterStrategy implements CreatorStrategy {
   public color = trueColor(237, 41, 69);
   public variants?: Variant[] | undefined = [];
   public dependencies = {
-    "@geektech/nest-core": "^3.0.0",
+    "@geektech/nest-core": "^3.2.0",
   };
   public devDependencies = {};
   public scripts = {
